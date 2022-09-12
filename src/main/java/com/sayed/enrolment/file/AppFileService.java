028b829616a1a3693d2936bdd9824a07719aecf8
Kirill Sayed <sayed.kirill@gmail.com>
2022-09-12 11:12:37 +0300

    Added /updated realization

--- src/main/java/com/sayed/enrolment/file/AppFileService.java
+++ src/main/java/com/sayed/enrolment/file/AppFileService.java
@@ -5,10 +5,12 @@ import com.sayed.enrolment.file.exceptions.AppFileNotFoundException;
 import com.sayed.enrolment.folder.exceptions.FolderNotFoundException;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public interface AppFileService {
     void saveFile(EntityDto dto, Timestamp updateDate) throws FolderNotFoundException;
     AppFile getFile(String id) throws AppFileNotFoundException;
+    List<AppFile> updates(Timestamp date);
     boolean fileDuplicateCheck(String id);
     void deleteFile(String id, Timestamp date) throws AppFileNotFoundException;
 }
